6e300108b4af7b48243e3b8d80b1a1b9a4e1848c
Sajeev Raj <sajeevraj.1989@gmail.com>
2019-04-21 20:34:57 +0400

    Initial Commit

---
+++ RxSwift-MVVM-SampleTests/RxSwift_MVVM_SampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  RxSwift_MVVM_SampleTests.swift
+//  RxSwift-MVVM-SampleTests
+//
+//  Created by sajeev Raj on 4/21/19.
+//  Copyright © 2019 Sajeev. All rights reserved.
+//
+
+import XCTest
+@testable import RxSwift_MVVM_Sample
+
+class RxSwift_MVVM_SampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
